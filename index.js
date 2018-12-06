690d1d8d559efd1d05f4fe2be9e7cd5874efec8c
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-12-06 06:20:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code in this file!
+
+
+function scuberGreetingForFeet() {
+  
+  if ride < 400
+  return 'This one is on me!'
+  else if ride > 2000 and ride < 5000
+  return 'I will gladly take your thirty bucks.'
+  
+  
+}
+
+function ternaryCheckCity(){
+  
+  
+}
+
+function switchOnCharmFromTip(){
+  
+  
+  
+  
+}
--- test/indexTest.js
+++ test/indexTest.js
@@ -5,11 +5,7 @@ describe('index.js', function () {
     it('gives customers a free sample if the ride is less than or equal to 400 feet', function () {
       expect(scuberGreetingForFeet(199)).to.equal('This one is on me!');
     });
-
-    it('charges 30 dollars for a distance over 2000 feet', function () {
-      expect(scuberGreetingForFeet(2001)).to.equal('I will gladly take your thirty bucks.');
-    });
-
+ 
     it('does not allow rides over 2500 feet', function () {
       expect(scuberGreetingForFeet(2501)).to.equal('No can do.');
     });
