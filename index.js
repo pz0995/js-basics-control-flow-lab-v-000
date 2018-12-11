274f57c0b0a2ced8edf68919c9d08179b5181e1e
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-12-11 11:30:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code in this file!
+
+
+// function scuberGreetingForFeet() {
+
+  if (ride <= 400) {
+    console.log('This one is on me!');
+  else if (ride > 2000) AND (ride <= 2500)
+    console.log('I will gladly take your thirty bucks.');
+  else if (ride > 5000)
+    console.log( "No can do.");
+  
+// }
+
+// function ternaryCheckCity(city){
+  
+  city.NYC = (true ? 'Ok, sounds good.' : "No go.");
+// }
+
+// function switchOnCharmFromTip() {
+  switch
+    case (generous):
+      return "Thank you so much.";
+      break;
+    case not as generous:
+      return "Thank you.";
+      break;
+    default:
+      return 'Bye.';
+  // }
+// };
